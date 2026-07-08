28ee4e70f1b19f9de331c9d9f3bf4c103ff81c1f
Citizen MP <citizen@example.com>
2026-07-09 00:13:14 +0530

    Configure Next.js static and Github Actions deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/peoples-priorities-portal',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
